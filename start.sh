e5c4d2f2202a0f097f149f2a9fe14f5b4bd9c8b8
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2015-03-06 15:51:55 +0100

    Automatically install Magento

--- start.sh
+++ start.sh
@@ -34,6 +34,29 @@ echo "Adding Magento Caching"
 
 sed -i -e  '/<\/config>/{ r /var/www/app/etc/mage-cache.xml' -e 'd}' /var/www/app/etc/local.xml.template
 
+
+echo "Installing"
+php -f /var/www/install.php -- \
+--license_agreement_accepted yes \
+--locale "fr_FR" \
+--timezone "Europe/Berlin" \
+--default_currency "EUR" \
+--db_host "db" \
+--db_name "magento" \
+--db_user "$DB_ENV_USER" \
+--db_pass "$DB_ENV_PASS" \
+--url "http://127.0.0.1/" \
+--skip_url_validation \
+--use_rewrites no \
+--use_secure no \
+--secure_base_url "" \
+--use_secure_admin no \
+--admin_firstname "Admin" \
+--admin_lastname "Admin" \
+--admin_email "admin@admin.com" \
+--admin_username "admin" \
+--admin_password "admin25"
+
 touch /firstrun
 
 fi
