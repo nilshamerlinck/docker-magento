521d5f699c78444b613395ab25966595bc69cd06
Nils Hamerlinck <nils@hamerlinck.fr>
2018-04-24 19:54:18 +0700

    [FIX] expected db hostname is 'db'

--- seturl.php
+++ seturl.php
@@ -1,6 +1,6 @@
 <?php
 $httphost = ($_SERVER['HTTP_HOST']);
-mysql_connect('mariadb',$_SERVER['DB_ENV_USER'], $_SERVER['DB_ENV_PASS']) or die ('mysql error');
+mysql_connect('db',$_SERVER['DB_ENV_USER'], $_SERVER['DB_ENV_PASS']) or die ('mysql error');
 mysql_select_db('magento');
 mysql_query('update core_config_data set value="http://'.$httphost.'/" where path = "web/unsecure/base_url"');
 mysql_query('update core_config_data set value="http://'.$httphost.'/" where path = "web/secure/base_url"');
